a3af38d238107fdfbedca5bfb0f07dc3b5b1fe21
Adnan-Raja <adnan.saeed.raja@gmail.com>
2021-11-22 19:46:46 -0500

    head added

--- head.js
+++ head.js
@@ -7,10 +7,11 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(4, 3);
-
-
+const head = function(array) {
+  return array[0];
+};
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([6,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hi");
+assertEqual(head([]), "Hello");
