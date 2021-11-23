86a85af30b45bde4fe456393d048183c51a2e11b
Adnan-Raja <adnan.saeed.raja@gmail.com>
2021-11-22 19:04:38 -0500

    commit assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("\u{1F44D}\u{1F44D}\u{1F44D} Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("\u{1F44E}\u{1F44E}\u{1F44E} Assertion Failed: " + actual + "  !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(4, 3);
+
+
