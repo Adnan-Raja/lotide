1c7db67a9569e48fd5057bbe409b52d9f0423253
Adnan-Raja <adnan.saeed.raja@gmail.com>
2021-11-22 21:26:16 -0500

    added tail

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,7 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`\u{1F44D}\u{1F44D}\u{1F44D} Assertion Passed: ${actual}=== ${expected}`);
+    console.log(`\u{1F44D}\u{1F44D}\u{1F44D} Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`\u{1F44E}\u{1F44E}\u{1F44E} Assertion Failed: ${actual} !== ${expected}`);
   }
