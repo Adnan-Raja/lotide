46a7e68742f59d19bbaa680d751fa41c8246195a
Adnan-Raja <adnan.saeed.raja@gmail.com>
2021-11-23 22:09:56 -0500

    assert Array Equal

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+const eqArrays = function(arr1, arr2){
+  if (arr1.length !== arr2.length) 
+    return false;
+       for (var i = 0; i < arr1.length; i++){
+        if (arr1[i] !== arr2[i]){
+            return false;
+        }
+    }
+    return true;   
+};
+
+const assertArraysEqual = function (ary1, ary2){
+  if(eqArrays(ary1, ary2)){
+    console.log(`yes, it is ${eqArrays(ary1, ary2)}`);
+  } else { 
+    console.log(`No, it is ${eqArrays(ary1, ary2)}`);
+  }
+  
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
