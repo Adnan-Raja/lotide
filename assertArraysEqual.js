f0f734c875e707828d845ce05ebcc88dc179efd4
Adnan-Raja <adnan.saeed.raja@gmail.com>
2021-11-29 18:14:07 -0500

    assetArrayEqual and its test

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length){ 
-    return false;
-  }
-       for (var i = 0; i < arr1.length; i++){
-        if (arr1[i] !== arr2[i]) {
-            return false;
-        }
-    }
-    return true;   
-};
-
+const eqArrays = require('./eqArrays');
 const assertArraysEqual = function (ary1, ary2){
   if(eqArrays(ary1, ary2)){
     console.log(`yes, it is ${eqArrays(ary1, ary2)}`);
@@ -19,8 +8,10 @@ const assertArraysEqual = function (ary1, ary2){
   
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
+// assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
+// assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
 
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
+// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
+// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
+
+module.exports = assertArraysEqual;
