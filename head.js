e9b398898c0bdfec92b247625b9dc5542efae51a
Adnan-Raja <adnan.saeed.raja@gmail.com>
2021-11-29 17:06:22 -0500

    updated assertEqual, head and also included test directory

--- head.js
+++ head.js
@@ -1,11 +1,5 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`\u{1F44D}\u{1F44D}\u{1F44D} Assertion Passed: ${actual}=== ${expected}`);
-  } else {
-    console.log(`\u{1F44E}\u{1F44E}\u{1F44E} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   return array[0];
