da3916a7b41159e2b804353f964e37ed5eddb582
Adnan-Raja <adnan.saeed.raja@gmail.com>
2021-11-29 17:41:40 -0500

    updated head and new tail

--- head.js
+++ head.js
@@ -4,8 +4,10 @@ const assertEqual = require('./assertEqual');
 const head = function(array) {
   return array[0];
 };
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([6,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hi");
-assertEqual(head([]), "Hello");
+//assertEqual(head([5,6,7]), 5);
+//assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+//assertEqual(head([6,6,7]), 5);
+//assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hi");
+//assertEqual(head([]), "Hello");
+
+module.exports = head;
--- tail.js
+++ tail.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`\u{1F44D}\u{1F44D}\u{1F44D} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`\u{1F44E}\u{1F44E}\u{1F44E} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const tail = function(arr) {
   let res = [];
@@ -13,12 +7,14 @@ const tail = function(arr) {
   }
   return res;
 };
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-assertEqual(words[0], "labs");
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+// assertEqual(words[0], "labs");
+
+module.exports = tail;
